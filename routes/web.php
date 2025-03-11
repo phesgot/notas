6988509c8320229993f47e06956ee534681341b3
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-11 10:36:57 -0300

    002

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,7 @@ Route::get('/', function () {
 Route::get('/about', function(){
     echo 'About us';
 });
+
+Route::get('/main/{value}', [MainController::class, 'index']);
+Route::get('/page2/{value}', [MainController::class, 'page2']);
+Route::get('/page3/{value}', [MainController::class, 'page3']);
