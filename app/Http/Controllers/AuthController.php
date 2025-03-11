ed3a6f28e964c0cdcc3678f3030176f7047adc31
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-11 15:31:10 -0300

    005 - Implementado token de segurança no form

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,11 @@ class AuthController extends Controller
         return view('login');
     }
 
+    public function loginSubmit(Request $request)
+    {
+        echo 'login submit';
+    }
+
     public function logout()
     {
         echo 'logout';
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,7 +13,8 @@
                 <!-- form -->
                 <div class="row justify-content-center">
                     <div class="col-md-10 col-12">
-                        <form action="#" method="post">
+                        <form action="/loginSubmit" method="post">
+                            @csrf <!--insere um token no formulário por questões de segurança -->
                             <div class="mb-3">
                                 <label for="text_username" class="form-label">Username</label>
                                 <input type="text" class="form-control bg-dark text-info" name="text_username" required>
