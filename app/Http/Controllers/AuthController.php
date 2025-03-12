01b5d6bf8735bf6099ea49afb141c4ee16154b58
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-12 11:29:40 -0300

    013 - Models

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -35,15 +36,14 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        // test database connection
-        try {
-            DB::connection()->getPdo();
-            echo "Connection is OK!";
-        } catch (\PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
+        // get all the user from the database
+        //$users = User::all()->toArray();
 
-        echo "FIM!";
+        // as an object instance of the model`s class
+        $userModel = new User();
+        $users = $userModel->all()->toArray();
+        echo '<pre>';
+        print_r($users);
     }
 
     public function logout()
