0ec72e87c91a16d1a140999dcf95dda79e311280
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-13 10:41:01 -0300

    017 - Cria página home

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -70,7 +70,8 @@ class AuthController extends Controller
             ]
             ]);
 
-        echo 'LOGIN COM SUCESSO!';
+        // redirect to home
+        return redirect()->to('/');
     }
 
     public function logout()
