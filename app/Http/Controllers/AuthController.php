c650464af25e0b272ed724c5bb2dfb820e428868
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-12 21:01:39 -0300

    014 - autenticação basica de user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use function Laravel\Prompts\password;
+
 class AuthController extends Controller
 {
     public function login()
@@ -36,14 +38,39 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        // get all the user from the database
-        //$users = User::all()->toArray();
+        // check if user exists 
+        $user = User::where('username', $username)
+                ->where('deleted_at', NULL)
+                ->first();
 
-        // as an object instance of the model`s class
-        $userModel = new User();
-        $users = $userModel->all()->toArray();
-        echo '<pre>';
-        print_r($users);
+        if(!$user){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Username ou password incorreto.');
+        }
+
+        // check if password is correct 
+        if(!password_verify($password, $user->password)){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Username ou password incorreto.');
+        }
+
+        // update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        // login user
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+            ]);
+
+        echo 'LOGIN COM SUCESSO!';
     }
 
     public function logout()
