e2599cd498af42abbbb72316217346ee975ccaa8
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-14 15:55:26 -0300

    025 - criando um service para decripitar

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -26,24 +27,13 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "I'm editing note with id = $id";
     }
 
     public function deleteNote($id)
     {
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "I'm deleting note with id = $id";
-    }
-
-    private function decryptId($id)
-    {
-        // check if $id is encrypted
-        try {
-            $id =    Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect()->route('home');
-        }
-        return $id;
     }
 }
