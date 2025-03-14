2d306666953f0904928203d808c823882a20f7f4
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-14 15:24:49 -0300

    024 - Otimização do código

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -26,23 +26,24 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect()->route('home');
-        }
-
+        $id = $this->decryptId($id);
         echo "I'm editing note with id = $id";
     }
 
     public function deleteNote($id)
     {
+        $id = $this->decryptId($id);
+        echo "I'm deleting note with id = $id";
+    }
+
+    private function decryptId($id)
+    {
+        // check if $id is encrypted
         try {
-            $id = Crypt::decrypt($id);
+            $id =    Crypt::decrypt($id);
         } catch (DecryptException $e) {
             return redirect()->route('home');
         }
-
-        echo "I'm deleting note with id = $id";
+        return $id;
     }
 }
