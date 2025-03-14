d2149a4c1a98f709ee4a0793b3f4f8c5698e8091
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-14 15:16:46 -0300

    023 - capturando e validando os id's encriptados

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,21 @@ use App\Http\Middleware\CheckIsLogged;
 use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-// auth routes
+// auth routes - user not logged
 Route::middleware([CheckIsNotLogged::class])->group(function(){
     Route::get('/login', [AuthController::class, 'login']);
     Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 });
 
+// app routes - user logged
 Route::middleware([CheckIsLogged::class])->group(function(){
     Route::get('/', [MainController::class, 'index'])->name('home');
     Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
+
+// edit note
+Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
+// delete note
+Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
 
