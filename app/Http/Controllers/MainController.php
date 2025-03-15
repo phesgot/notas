6b7c83ca6360647b69839b4200bb0949751ec8fb
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-15 12:28:32 -0300

    026 - Cria formulário de new note

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -22,7 +22,13 @@ class MainController extends Controller
     }
     public function newNote()
     {
-        echo "I'm creating a new note!";
+        // show new note view 
+        return view('new_note');
+    }
+
+    public function newNoteSubmit(Request $request)
+    {
+        echo "I'm creating a new note";
     }
 
     public function editNote($id)
--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,22 @@ use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
 // auth routes - user not logged
-Route::middleware([CheckIsNotLogged::class])->group(function(){
+Route::middleware([CheckIsNotLogged::class])->group(function () {
     Route::get('/login', [AuthController::class, 'login']);
     Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 });
 
 // app routes - user logged
-Route::middleware([CheckIsLogged::class])->group(function(){
+Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index'])->name('home');
     Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
+    Route::post('/newNoteSubmit', [MainController::class, 'newNoteSubmit'])->name('newNoteSubmit');
+    
+
+    // edit note
+    Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
+    // delete note
+    Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
+
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
-
-// edit note
-Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
-// delete note
-Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
-
