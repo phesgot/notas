8a598ef581496f663f15e677eff9c2039b5e3606
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-03-17 10:15:31 -0300

    032 - Otimiza o SoftDelete

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -137,8 +137,14 @@ class MainController extends Controller
         // $note->delete();
 
         // 2. soft delete
-        $note->deleted_at = date('Y:m:d H:i:s');
-        $note->save();
+        // $note->deleted_at = date('Y:m:d H:i:s');
+        // $note->save();
+
+        // 3. soft delete (property SoftDeletes in model)
+        $note->delete();
+
+        // 4. hard delete (property SoftDeletes in model)
+        // $note->forceDelete();
 
         // redirect to home
         return redirect()->route('home');
